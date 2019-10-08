30f64913588c1d0eb62f87322dfe0bc352ba3067
devbrock <tbrockb@gmail.com>
2019-10-08 07:59:23 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  let name = "Brock";
+  let age = 22;
+
+  $: uppercaseName = name.toUpperCase();
+
+  function upAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "timmy";
+  }
+</script>
+
+<style>
+  h1 {
+    color: blue;
+  }
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={upAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
